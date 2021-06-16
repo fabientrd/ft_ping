f2291cf08c871dc78e9a9c4f1d97b8e9e6760076
fabientrd <fabientreand@gmail.com>
2021-06-16 12:19:24 -0400

    debut

---
+++ includes/ft_ping.h
@@ -0,0 +1,15 @@
+#ifndef FT_PING_H
+# define FT_PING_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <netdb.h>
+# include <sys/time.h>
+# include <arpa/inet.h>
+# include <signal.h>
+
+
+# endif
---
+++ src/main.c
@@ -0,0 +1,5 @@
+#include "../includes/ft_ping.h"
+
+int		main(int ac, char **av){
+	return (0);
+}
